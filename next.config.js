534b86f6aa5047f1c1319db5e5bdba58df704e7d
Theo Costea <theo.costea@gmail.com>
2022-06-30 19:09:11 +0200

    Suppress type errors on build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    // Remove this after switching to TypeScript
+    ignoreBuildErrors: true,
+  },
 };
