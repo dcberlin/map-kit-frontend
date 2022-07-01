8e0e5c4bc45ce0e15fa5ad2a1619401fb2f672b5
Theo Costea <theo.costea@gmail.com>
2022-07-01 15:10:20 +0200

    Improve aesthetics of POI drawer

--- components/poi-drawer.tsx
+++ components/poi-drawer.tsx
@@ -4,8 +4,9 @@ import {
   LocationMarkerIcon,
   MailIcon,
   PhoneIcon,
-  LinkIcon,
+  ExternalLinkIcon,
 } from "@heroicons/react/solid";
+import { XCircleIcon } from "@heroicons/react/outline";
 
 /**
  * Map with POIs. The initial viewport is set to the specified bbox
@@ -17,13 +18,19 @@ export default function MapWidget({ locations, bbox }) {
 
   return (
     selectedPoi && (
-      <div className="fixed w-screen bottom-0 z-30 bg-white p-8 sm:w-1/3 sm:left-1/3 sm:mb-5 drop-shadow-xl rounded">
+      <div className="fixed w-screen sm:w-1/2 sm:left-1/4 bottom-0 sm:mb-5 z-30 bg-white p-8 drop-shadow-xl rounded">
         <div className="flex flex-col justify-start">
-          <h2 className="font-bold text-gray-700">{name}</h2>
-          <div className="mt-5 text-md">
+          <div className="flex justify-between">
+            <h2 className="font-bold text-gray-700">{name}</h2>
+            <XCircleIcon
+              className="w-8 h-8 text-gray-400 hover:text-red-400 cursor-pointer"
+              onClick={() => setSelectedPoi(null)}
+            />
+          </div>
+          <div className="flex flex-col gap-1 mt-5 text-md">
             {address && (
               <div className="flex items-center font-semibold text-gray-600">
-                <LocationMarkerIcon className="flex-none inline h-5 w-5 mr-2" />
+                <LocationMarkerIcon className="flex-none inline h-5 w-5 mr-2 text" />
                 <div>{address}</div>
               </div>
             )}
@@ -42,15 +49,15 @@ export default function MapWidget({ locations, bbox }) {
               </div>
             )}
             {website && (
-              <div className="flex items-center">
-                <LinkIcon className="flex-none h-5 w-5 mr-2 text-gray-600" />
+              <div className="flex items-center text-gray-600 mt-1">
+                <ExternalLinkIcon className="flex-none h-5 w-5 mr-2" />
                 <a
                   href={website}
                   target="_blank"
                   rel="noreferrer"
-                  className="text-blue-600"
+                  className="px-3 py-1 gap-1 bg-blue-200 rounded-2xl text-xs font-bold uppercase"
                 >
-                  {website}
+                  website
                 </a>
               </div>
             )}
