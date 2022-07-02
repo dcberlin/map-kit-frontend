91458fd714e1fb1064980d49aee2d800da23d1e2
Theo Costea <theo.costea@gmail.com>
2022-07-02 18:28:30 +0200

    Improve error & loading screens layout

--- components/error-screen.tsx
+++ components/error-screen.tsx
@@ -2,8 +2,10 @@ import { EmojiSadIcon } from "@heroicons/react/solid";
 
 export default function ErrorScreen() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <EmojiSadIcon className="w-8 h-8 text-red-400" />
+    <div className="fixed h-full w-full top-0 left-0 bg-white opacity-70 z-50">
+      <div className="flex h-full w-full items-center justify-center">
+        <EmojiSadIcon className="w-8 h-8 text-red-600" />
+      </div>
     </div>
   );
 }
--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -2,8 +2,10 @@ import { GlobeIcon } from "@heroicons/react/solid";
 
 export default function LoadingScreen() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <GlobeIcon className="w-8 h-8 text-gray-600 animate-bounce" />
+    <div className="fixed h-full w-full top-0 left-0 bg-white z-50 opacity-70">
+      <div className="flex h-full w-full items-center justify-center">
+        <GlobeIcon className="w-8 h-8 text-gray-700 animate-bounce" />
+      </div>
     </div>
   );
 }
