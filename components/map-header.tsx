f5db64847199864cb2114befae05691bad125033
Theo Costea <theo.costea@gmail.com>
2023-10-27 13:32:46 +0200

    Replace old map logo on community page as well

--- components/map-header.tsx
+++ components/map-header.tsx
@@ -1,47 +1,57 @@
-import {MapIcon} from "@heroicons/react/solid";
+import { MapIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 import Link from "next/link";
-import {Community} from "../models";
+import { Community } from "../models";
 
 interface MapHeaderProps {
-    community?: Community;
-    inAdminScreen?: boolean;
+  community?: Community;
+  inAdminScreen?: boolean;
 }
 
-const MapHeader = ({community, inAdminScreen}: MapHeaderProps) => (
-    <div className="fixed left-5 top-5 z-20 text-gray-700">
-        <div className="flex justify-center items-center gap-2">
-            <MapIcon className="text-red-500 h-8 w-8 opacity-90" />
-            <h1>
-                <Link href="/">
-                    <span className="hover:text-red-400">Harta Diasporei</span>
-                </Link>
-                {inAdminScreen &&
-                  <>
-                    &nbsp;&#187;&nbsp;
-                    <Link href={"/my-communities"}>
-                      <span className="hover:text-red-400">Admin</span>
-                    </Link>
-                  </>
-                }
-                {community &&
-                    (!inAdminScreen ? (
-                        <>
-                            <span> din </span>
-                            <Link href={"/" + community.path_slug}>
-                                <span className="font-bold hover:text-orange-500">{community.name}</span>
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            &nbsp;&#187;&nbsp;
-                            <Link href={"/" + community.path_slug}>
-                                <span className="font-bold hover:text-orange-500">{community.name}</span>
-                            </Link>
-                        </>
-                    ))}
-            </h1>
-        </div>
+const MapHeader = ({ community, inAdminScreen }: MapHeaderProps) => (
+  <div className="fixed left-5 top-5 z-20 text-gray-700">
+    <div className="flex justify-center items-center gap-2">
+      <Image
+        src="/logo-dcb.png"
+        alt="Logo al Harta Diasporei"
+        width={50}
+        height={50}
+      />
+      <h1>
+        <Link href="/">
+          <span className="hover:text-red-400">Harta Diasporei</span>
+        </Link>
+        {inAdminScreen && (
+          <>
+            &nbsp;&#187;&nbsp;
+            <Link href={"/my-communities"}>
+              <span className="hover:text-red-400">Admin</span>
+            </Link>
+          </>
+        )}
+        {community &&
+          (!inAdminScreen ? (
+            <>
+              <span> din </span>
+              <Link href={"/" + community.path_slug}>
+                <span className="font-bold hover:text-orange-500">
+                  {community.name}
+                </span>
+              </Link>
+            </>
+          ) : (
+            <>
+              &nbsp;&#187;&nbsp;
+              <Link href={"/" + community.path_slug}>
+                <span className="font-bold hover:text-orange-500">
+                  {community.name}
+                </span>
+              </Link>
+            </>
+          ))}
+      </h1>
     </div>
+  </div>
 );
 
 export default MapHeader;
