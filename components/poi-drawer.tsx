286d70940ff8db23264f050dcb3e3d2cd346351c
Theo Costea <theo.costea@gmail.com>
2024-06-13 14:33:39 +0200

    Change label and swap directions on Google Maps link

--- components/poi-drawer.tsx
+++ components/poi-drawer.tsx
@@ -218,12 +218,12 @@ export default function PoiDrawer({community, categories}: PoiDrawerProps) {
               <div className="flex items-center font-semibold text-gray-600">
                 <MapIcon className="flex-none inline h-5 w-5 mr-3 text" />
                 <a
-                    href={`https://www.google.com/maps/dir/${address}`}
+                    href={`https://www.google.com/maps/dir//${address}`}
                     target="_blank"
                     rel="noreferrer"
                     className="px-3 py-1 gap-1 bg-blue-200 rounded-2xl text-xs font-bold uppercase"
                 >
-                  deschide harta
+                  Navigaţie Google
                 </a>
               </div>
           )}
